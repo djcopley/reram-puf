65a0c897cb67c120e9be54a22ab8abbeef2d8995
Daniel Copley <djcopley@users.noreply.github.com>
2021-03-21 16:57:48 -0700

    Updated reram

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     version="1.0.0",
     license="GPLv3",
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=[
         "pyserial",
     ],
