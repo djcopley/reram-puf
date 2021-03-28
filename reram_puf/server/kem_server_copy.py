3503544e2375e3609963cee5c5c125bd2846541e
cac765 <cac765@nau.edu>
2021-03-28 11:56:50 -0600

    copies for testing and development

--- reram_puf/server/kem_server_copy.py
+++ reram_puf/server/kem_server_copy.py
@@ -17,6 +17,7 @@
 import os
 import struct
 import getpass
+import hashlib
 
 """KEM Server Class for creating a server instance for communication."""
 
@@ -30,19 +31,60 @@ class KEMServer:
         self._addresses = []
         self._orders = []
 
-    def authenticate(self, client: dict) -> Generator[str,bool]:
+    def authenticate(self, user: dict, passwd=None) -> list:
         """Authenticate a user with client data."""
-        passwd = getpass.getpass()
+        if passwd is None:
+            passwd = getpass.getpass()
         yield passwd
 
-        pwd_hash = self.hash(passwd, client["salt"])
-        if pwd_hash == client["key"]:
+        pwd_hash = self.hash(passwd, user["salt"])
+        if pwd_hash == user["key"]:
             print("[SUCCESS]: Authentication Successful")
             yield True
         else:
             print("[ERROR]: Authentication failed. Invalid username or password.")
             yield False
 
-    def enroll(self) -> bool:
+    def enroll(self, user=None, passwd=None, salt=None) -> tuple:
         """Enroll a new client."""
-        pass
+        if user is None:
+            user = input("Enter username: ")
+
+        if passwd is None:
+            pass_confirmed = False
+            strikes = 0
+            while not pass_confirmed and strikes < 3:
+                password1 = getpass.getpass(prompt="Enter Password:")
+                password2 = getpass.getpass(prompt="Confirm Password:")
+                if password1 == password2:
+                    pass_confirmed = True
+                else:
+                    print("[ERROR]: Passwords do not match.")
+                    strikes += 1
+            if strikes == 3:
+                return None
+        if salt is None:
+            salt = self.generate_salt(self.salt_len)
+        pwd_hash = self.hash(passwd, salt)
+        return user, pwd_hash, salt
+
+    def generate_salt(self, length: int) -> bytes:
+        """Generate a new salt for handshake or enrollment."""
+        return os.urandom(length)
+
+    def handshake(self, user: dict, passwd=None) -> bytes:
+        """Handshake process with a client."""
+        [passwd, auth] = self.authenticate(user, passwd=passwd)
+        if auth:
+            rand = self.generate_salt(self.group_len)
+            print(f"[HANDSHAKE]: {rand}")
+            return rand
+        print("[HANDSHAKE]: Failed.")
+        return None
+
+    def hash(self, message: str, salt: bytes) -> str:
+        """Conduct SHA-256 Hash on message with salt."""
+        digest = hashlib.sha256()
+        msg = bytes(message, "utf-8") + salt
+        digest.update(msg)
+        return digest.hexdigest()
---
+++ reram_puf/server/test_kem_server.py
@@ -0,0 +1,38 @@
+import unittest
+from kem_server_copy import KEMServer
+
+class TestKEMServerMethods(unittest.TestCase):
+    
+    def setUp(self):
+        self.ks = KEMServer(salt_len=16, group_len=2)
+
+    def test_hash(self):
+        salt = b'\xa0\xd5\x08\xb9\x8au\xf7:\x81\x1dC\xc8J\xb7\xfd`'
+        test_hash = \
+            'edaf35d9626155406a6b7ad04e4661451ed90f3dc5262632ebfaaaf65a119485'
+        real_hash = self.ks.hash("test", salt)
+        self.assertEqual(test_hash, real_hash)
+    
+    def test_authenticate(self):
+        client = { 
+            "key" : 
+            '9389203d0b9aaa2ae452626dd522b1a00bc15d481597c22bd4d3ae6b1fe4f071',
+            "salt" : b'\xa0\xd5\x08\xb9\x8au\xf7:\x81\x1dC\xc8J\xb7\xfd`'
+            }
+        [passwd, auth] = self.ks.authenticate(client, passwd="test_passwd")
+        self.assertEqual(passwd, "test_passwd")
+        self.assertTrue(auth)
+
+    def test_enroll(self):
+        test_salt = self.ks.generate_salt(self.ks.salt_len)
+        test_hash = self.ks.hash("password", test_salt)
+        results = self.ks.enroll(user="user", passwd="password", salt=test_salt)
+        user, passwd, salt = results
+        self.assertEqual(user, "user")
+        self.assertEqual(passwd, test_hash)
+        self.assertEqual(salt, test_salt)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
