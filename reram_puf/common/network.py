1962e35355159b078fdd00edde35e6af5f4f1b6b
cac765 <cac765@nau.edu>
2021-03-31 12:02:08 -0600

    added network wrapper functions and mqtt communication support

--- reram_puf/common/network.py
+++ reram_puf/common/network.py
@@ -1,12 +1,42 @@
+######## Client Manager Utilities ########
+#
+# Authors: Corey Cline
+#          Daniel Copley
+#
+# Date: 03/22/2021
+#
+# Description:
+# A Network interface class for MQTT plug and play communication.
+#
+#############################################################################
+from mqtt_client import MQTTClient
+
+"""Network Class for MQTT Communication between client and server."""
+
+
 class Network:
     
-    def __init__(self):
-        pass
+    def __init__(self, hostname: str, user: str):
+        """Constructor method."""
+        self.hostname = hostname
+        self.user = user
+        self.mqtt = MQTTClient(self.hostname, self.user)
+        self.unread = self.mqtt.msg_queue
 
-    def send(self, msg: str) -> bool:
-        """Send a message, return true if successful."""
-        pass
+    def close(self, port=1883):
+        """Close the network connection."""
+        self.mqtt.loop_stop()
+        self.mqtt.disconnect()
 
-    def receive(self) -> str:
+    def connect(self):
+        """Connect to the network interface."""
+        self.mqtt.connect(port)
+        self.mqtt.loop_start
+
+    def receive(self, user=self.user) -> str:
         """Receive a message and return the string."""
-        pass
+        self.mqtt.subscribe(topic=user)
+
+    def send(self, receiver: str, msg: bytes) -> bool:
+        """Send a message, return true if successful."""
+        self.mqtt.publish(topic=receiver, msg)
