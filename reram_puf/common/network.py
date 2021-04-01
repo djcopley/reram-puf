60be82acab0621773bf2394fc95ebb9958061eb9
cac765 <cac765@nau.edu>
2021-04-01 00:29:55 -0600

    resolved network issues, pub/sub working in main

--- reram_puf/common/network.py
+++ reram_puf/common/network.py
@@ -10,6 +10,7 @@
 #
 #############################################################################
 from reram_puf.common.mqtt_client import MQTTClient
+import time
 
 """Network Class for MQTT Communication between client and server."""
 
@@ -21,9 +22,8 @@ class Network:
         self.hostname = hostname
         self.user = user
         self.mqtt = MQTTClient(self.hostname, self.user)
-        self.unread = self.mqtt.msg_queue
 
-    def close(self):
+    def disconnect(self):
         """Close the network connection."""
         self.mqtt.loop_stop()
         self.mqtt.disconnect()
@@ -31,14 +31,23 @@ class Network:
     def connect(self, port=1883):
         """Connect to the network interface."""
         self.mqtt.connect(port)
-        self.mqtt.loop_start
+        self.mqtt.loop_start()
 
     def receive(self, user=None) -> str:
         """Receive a message and return the string."""
         if user is None:
             user = self.user
         self.mqtt.subscribe(user)
+        print("Waiting for a message...")
+        cmd_received = False
+        while not cmd_received:
+            time.sleep(0.1) # Delay to throttle tight loop
+            if len(self.mqtt.msg_queue) > 0:
+                # Grab the message from the queue
+                incoming_msg = self.mqtt.msg_queue.pop()
+                return incoming_msg
+        
 
-    def send(self, receiver: str, msg: bytes) -> bool:
+    def send(self, receiver: str, msg: bytes):
         """Send a message, return true if successful."""
         self.mqtt.publish(receiver, msg, qos=2)
--- reram_puf/server/main.py
+++ reram_puf/server/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from reram_puf import __version__
 from reram_puf.common.network import Network
@@ -8,12 +9,8 @@ from reram_puf.common.mqtt_client import MQTTClient
 def on_message( client, userdata, message ):
     """Callback function for receiving messages."""
     msg = message.payload.decode( "utf-8" )
-    client.msg_queue.append( msg )
-    logging.info( "\n\tTopic: {}\n\tMessage: {}\n\tRetained: {}".format(
-                  message.topic,msg, message.retain ) )
-
-    if ( message.retain == 1 ):
-        logging.info( "This was a retained message." )
+    # Capture the message in the global Network Class queue
+    network.mqtt.msg_queue.append( msg )
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -25,20 +22,16 @@ def parse_args():
         help="Name of the client who is connecting to the network.")
     return parser.parse_args()
 
-
 def main():
     args = parse_args()
+    global network # Declare global for on_message to access the queue
     network = Network(args.host, args.client)
+    network.mqtt.client.on_message = on_message
     network.connect()
-    msg = "Hello from Network"
-    network.send("test", msg)
+    incoming = network.receive("server/receive")
+    print(f"Incoming: {incoming}")
+    network.disconnect()
 
-    msg_2 = "Hello from MQTTClient"
-    client = MQTTClient("192.168.0.2", "kem_server")
-    client.client.on_message = on_message
-    client.connect()
-    client.loop_start()
-    client.publish("test", msg_2, qos=2, retain=False)
 
 
 if __name__ == '__main__':
