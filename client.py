4fa79d6ac027144d8040774d7636a5c24c01f673
Daniel Copley <djcopley@users.noreply.github.com>
2021-04-01 16:27:30 -0700

    Updated

--- client.py
+++ client.py
@@ -1,12 +1,39 @@
 import serial
 import struct
 import hashlib
-import getpass
+
+from getpass import getpass
 
 
 class Client:
     def __init__(self, port="/dev/tty.usbmodem14101", baudrate=115200):
         self.device = serial.Serial(port=port, baudrate=baudrate)
+        self.orders = []
+        self.addresses = (0, 1, 2, 3)
+
+    def handshake(self):
+        # Get password
+        password = getpass()
+        # Send password to server
+        # Get salt
+        salt = b""
+        # Hash the password with the salt
+        hashed_pw = self.sha_hash(password, salt)
+        # Generate instructions
+
+    @staticmethod
+    def sha_hash(password: str, salt: bytes) -> str:
+        """
+        Calculated SHA-256 hash
+
+        :param password:
+        :param salt:
+        :return:
+        """
+        digest = hashlib.sha256()
+        msg = bytes(password, "utf-8") + salt
+        digest.update(msg)
+        return digest.hexdigest()
 
     def decrypt(self, cipher):
         plain = ""
@@ -14,6 +41,7 @@ class Client:
 
     def puf_instr(self, addr: int, voltage: float):
         """
+        Issue and instruction to the PUF and read back the calculated current
 
         :param addr: ReRam cell address (0-3)
         :param voltage: Floating point voltage (0-5v)
