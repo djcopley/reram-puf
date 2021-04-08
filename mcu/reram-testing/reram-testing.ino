cae6c75914f1189e6e0a7005e29d63942d80cb42
cac765 <cac765@nau.edu>
2021-04-08 14:32:18 -0600

    bunches of testing

---
+++ mcu/reram-testing/reram-testing.ino
@@ -0,0 +1,182 @@
+const int wordLines[] = {10, 11};
+const int bitLines[] = {A0, A1};
+const int resistance = 5000; // Resistance of current sensing resistor
+const int rcDelay = 100; // time in ms to delay
+
+const byte numChars = 32;
+char receivedChars[numChars];
+bool newData = false;
+
+byte getAddress(word value)
+{
+    return (value >> 10) & 3;
+}
+
+word getInputData(word value)
+{
+    return value & 1023; // Mask top 6 bits to get lower 10 bits for voltage/current
+}
+
+float scaleVoltage(int voltage)
+{
+    return (voltage) * (5.0 / 1024.0);
+}
+
+void serialWriteFloat(float *buf)
+{
+    Serial.write((uint8_t *) buf, 4); // Write the float to the serial port
+    //Serial.println(*buf);
+}
+
+byte getInstruction(word value)
+{
+    return (value >> 12) & 1;
+}
+
+int readBinaryString(char *s) 
+{
+    int result = 0;
+    while(*s) 
+    {
+        result <<= 1;
+        if(*s++ == '1') result |= 1;
+    }
+    return result;
+}
+
+char * recvWithStartEndMarkers()
+{
+    static boolean inProgress = false;
+    static byte index = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char recvChar;
+
+    while (Serial.available() > 0 && newData == false)
+    {
+        recvChar = Serial.read();
+
+        if (inProgress)
+        {
+            if (recvChar != endMarker)
+            {
+                receivedChars[index] = recvChar;
+                index++;
+
+                if (index >= numChars)
+                {
+                    index = numChars - 1;
+                }
+            }
+            else
+            {
+                receivedChars[index] = '\0'; //terminate string
+                inProgress = false;
+                index = 0;
+                newData = true;
+            }
+        }
+
+        else if (recvChar == startMarker)
+        {
+            inProgress = true;
+        }
+    }
+    return receivedChars;
+}
+
+void showNewData()
+{
+    if (newData) 
+    {
+        //Serial.print(receivedChars);
+        newData = false;
+    }
+}
+
+float get_current_from_voltage(byte address, byte voltage)
+{
+    analogWrite(wordLines[address >> 1], voltage); // Write voltage to pin stored at top addr bit
+    delay(rcDelay);
+    int analog_raw = analogRead(bitLines[address & 1]);
+    //Serial.print("Analog Raw: ");
+    //Serial.println(analog_raw);
+    float current_in_amps = scaleVoltage(analog_raw) / resistance;
+    float current_in_uamps = current_in_amps * 1000000;
+    return current_in_uamps;
+}
+
+float get_voltage_from_current(byte address, word current)
+{
+    analogWrite(wordLines[address >> 1], 255); // Write full-scale to get line equation
+    delay(rcDelay);
+    int analog_raw = analogRead(bitLines[address & 1]);
+    //Serial.print("Analog Raw: ");
+    //Serial.println(analog_raw);
+    float r_voltage = scaleVoltage(analog_raw);
+    float pot_voltage = 5.0 - r_voltage;
+    //Serial.print("Pot Voltage: ");
+    //Serial.println(pot_voltage);
+    float current_in_amps = r_voltage / resistance;
+    float current_in_uamps = current_in_amps * 1000000;
+    // Serial.print("Max current in uA: ");
+    // Serial.println(current_in_uamps);
+    float pot_resistance = pot_voltage / current_in_amps;
+    // Serial.print("Pot Resistance: ");
+    // Serial.println(pot_resistance);
+    // Serial.print("Current to solve with: ");
+    // Serial.println(current);
+    float voltage_drop = pot_resistance * current / 1000000;
+    return voltage_drop + r_voltage;
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    for (int line = 0; line < 2; line++)
+    {
+        pinMode(wordLines[line], OUTPUT);
+    }
+}
+
+void loop()
+{
+    recvWithStartEndMarkers();
+    if (newData)
+    {
+        //String instr_str = receivedChars;
+        // Serial.print("Instruction: ");
+        // Serial.println(receivedChars);
+
+        //byte instr_word = atoi(receivedChars);
+        word input_raw = readBinaryString(receivedChars);
+        byte instr_byte = getInstruction(input_raw);
+        byte address = getAddress(input_raw);
+        word data_word = getInputData(input_raw);
+        // Serial.print("Integer data: ");
+        // Serial.println(data_word);
+
+        // Serial.print("Instruction bit: ");
+        // Serial.println(instr_byte);
+
+        if (!instr_byte)
+        {
+            // Serial.print("Address: ");
+            // Serial.println(address);
+            // Serial.print("Voltage to WL: ");
+            float voltage = data_word / 255.0 * 5.0;
+            // Serial.println(voltage);
+
+            float current_in_uamps = get_current_from_voltage(address, (byte)data_word);
+            // Serial.println("Current in uA: ");
+            Serial.println(current_in_uamps);
+        }
+        else
+        {
+            float voltage_out = get_voltage_from_current(address, data_word);
+            // Serial.println("Output Voltage: ");
+            Serial.println(voltage_out);
+        }
+        newData = false;
+    }
+}
--- reram_puf/client/test.py
+++ reram_puf/client/test.py
@@ -2,15 +2,72 @@ import struct
 import serial
 import time
 
-
 device = serial.Serial(port="COM4", baudrate=115200)
-device.read()
+device.timeout = 1
+device.flushInput()
+device.flushOutput()
+device.read_all()
 
-while True:
-    byte = int(input("Enter byte to write (binary): "), 2).to_bytes(1, "big")
-    device.write(byte)
+def enroll(device):
+    currents = {
+        400: "0110010000",
+        300: "0100101100",
+        200: "0011001000",
+        100: "0001100100" }
+    addresses = ["00","01","10","11"]
+    lut = {
+        400 : [],
+        300 : [],
+        200 : [],
+        100 : [] }
+    """
+    msg = input("Enter binary input to Arduino Serial: ")
+    #msg_int = int(msg, 2)
+    msg_bytes = f"<{msg}>".encode("utf-8")
+    device.write(msg_bytes)
     print("Waiting for response...")
-    #time.sleep(0.5)
-    read = device.read()
-    print("Response: {}".format(read))
+    time.sleep(0.5)
+    read = device.read_all()
+    out = read.decode("utf-8").split("\r\n")
+    print("Response: {}".format(out))
+    out = read.decode("utf-8").split("\r\n")[0]
+    out = float(out)
+    print(f"output: {out}")
+    """
+    for key, value in currents.items():
+        for addr in addresses:
+            msg = "0001"
+            msg += addr + value
+            print(f"MSG: {msg}")
+            msg_bytes = f"<{msg}>".encode("utf-8")
+            device.write(msg_bytes)
+            print("Waiting for response...")
+            time.sleep(0.5)
+            read = device.read_all()
+            out = read.decode("utf-8").split("\r\n")
+            print("Response: {}".format(out))
+            #voltage = float(voltage)
+            #lut[key].append(voltage)
+            break
+        break
+    return lut
+
+lut = enroll(device)
+print(lut)
+
+"""
+while True:
+    msg = input("Enter binary input to Arduino Serial: ")
+    msg_int = int(msg, 2)
+    msg_bytes = f"<{msg}>".encode("utf-8")
+    device.write(msg_bytes)
+    print("Waiting for response...")
+    time.sleep(0.5)
+    read = device.read_all()
+    out = read.decode("utf-8").split("\r\n")
+    print("Response: {}".format(out))
+    out = read.decode("utf-8").split("\r\n")[0]
+    out = float(out)
+    print(f"output: {out}")
     #print("Value converted to float: {}".format(struct.unpack("f", read)))
+"""
